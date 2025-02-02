3a169aff4f98f702ae52ad7e6ed13faf5fe1acf9
Eunjin <melong0523486@gmail.com>
2025-02-02 00:34:19 -0500

    set the username everytime the button is clicked. However, when trying to set a new name again, the prev name is not retained

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [username, setUsername] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+  function handleChange(event){
+    setSubmitted(false);
+    setUsername(event.target.value)
+  }
+
+  function handleButtonClick(){
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? username : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={username}/>
+        <button onClick={handleButtonClick}>Set Name</button>
       </p>
     </section>
   );
