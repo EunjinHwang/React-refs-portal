438910b140d0cc48429ef48d2b5ef74412af80bd
Eunjin <melong0523486@gmail.com>
2025-02-09 04:21:26 -0500

    imporve the modal to display the score when won, and show an appropriate msg when lost

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,14 @@
-function ResultModal({ref, result, targetTime}){
+function ResultModal({onReset, ref, remainingTime, targetTime}){
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
     return(
         <dialog ref={ref} className="result-modal">
-            <h2>You {result}</h2>
+            {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>The target time was <strong>{targetTime} second{targetTime > 1? 's': ''}.</strong></p>
-            <p>You stoped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+            <p>You stoped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -10,8 +10,11 @@ function TimerChallenge({ title, targetTime }) {
     
     if(timeRemaining <= 0){
         clearInterval(timer.current);
-        setTimeRemaining(targetTime * 1000);
         dialog.current.showModal();
+    }
+
+    function handleReset(){
+        setTimeRemaining(targetTime * 1000);
     }
     
     function handleStart() {
@@ -27,7 +30,7 @@ function TimerChallenge({ title, targetTime }) {
 
     return (
         <>
-            {<ResultModal ref={dialog} targetTime={targetTime} result="lost"/>}
+            {<ResultModal onReset={handleReset} ref={dialog} targetTime={targetTime} remainingTime={timeRemaining}/>}
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">
