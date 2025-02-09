261ca25d26984039e6e4f0bcb2d496c034bba379
Eunjin <melong0523486@gmail.com>
2025-02-09 03:46:57 -0500

    added ResultModal

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,14 @@
+function ResultModal({ref, result, targetTime}){
+    return(
+        <dialog ref={ref} className="result-modal">
+            <h2>You {result}</h2>
+            <p>The target time was <strong>{targetTime} second{targetTime > 1? 's': ''}.</strong></p>
+            <p>You stoped the timer with <strong>X seconds left.</strong></p>
+            <form method="dialog">
+                <button>Close</button>
+            </form>
+        </dialog>
+    );
+}
+
+export default ResultModal;
